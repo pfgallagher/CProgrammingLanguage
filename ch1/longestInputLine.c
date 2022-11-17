4a93defce3f9a1e9ff0cf750fd1be172e80f05c7
pfgallagher <pfgallagher@gmail.com>
2022-11-17 09:51:07 -0600

    Ch1 progress

--- ch1/longestInputLine.c
+++ ch1/longestInputLine.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
-#define MAXLINE 1000 /* maximum input line size */
+#define MAXLINE 20 /* maximum input line size */
 
-int getline(char line[], int maxline);
+int getLine(char line[], int maxline);
 void copy(char to[], char from[]);
+int checkLineOverflow(char line[], int len);
 
 /* print longest input line */
 int main() {
   int len;               /* current line length */
-  int max;               /* maximum length seen so far */
+  int max = 0;           /* maximum length seen so far */
   char line[MAXLINE];    /* current input line */
   char longest[MAXLINE]; /* longest line saved here */
-  max = 0;
-  while (0 < (len = getline(line, MAXLINE)))
+  while (0 < (len = getLine(line, MAXLINE))) {
+    len += checkLineOverflow(line, len);
     if (max < len) {
       max = len;
       copy(longest, line);
     }
+  }
   if (0 < max) /* there was a line */
-    printf("%s", longest);
+    printf("length %d: %s", max, longest);
+  if (MAXLINE < max) printf("...");
   return 0;
 }
 
-/* getline: read a line into s, return length */
-int getline(char s[], int lim) {
+/* getLine: read a line into s, return length */
+int getLine(char s[], int lim) {
   int c, i;
   for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) s[i] = c;
   if (c == '\n') {
@@ -39,4 +42,16 @@ void copy(char to[], char from[]) {
   int i;
   i = 0;
   while ((to[i] = from[i]) != '\0') ++i;
+}
+
+int checkLineOverflow(char line[], int len) {
+  int last = line[len - 1];
+  int overflow = 0;
+  if (last != '\n' && last != EOF) {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+      ++overflow;
+    }
+  }
+  return overflow;
 }
